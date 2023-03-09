9c46da5976455652fbf9a7b6c0e2bc8ab21ae439
Nurken Zhappargaly <zhnurken@gmail.com>
2023-03-10 00:05:38 +0600

    first commit

---
+++ src/store/index.js
@@ -0,0 +1,38 @@
+import { legacy_createStore as createStore, combineReducers, compose, applyMiddleware } from 'redux';
+import heroes from '../reducers/heroes';
+import filters from '../reducers/filters';
+
+const stringMiddleware = (store)=>(next)=>(action)=> {
+    if (typeof action === 'string') {
+        return next({
+            type: action
+        })
+    }
+    return next(action)
+}
+
+const enhancer = (createStore) => (...args) => {
+    const store  = createStore(...args);
+
+    const oldDispatch = store.dispatch;
+    store.dispatch = (action) => {
+        if (typeof action === 'string') {
+            return oldDispatch({
+                type: action
+            })
+        }
+        return oldDispatch(action)
+    }
+    return store;
+}
+
+
+const store = createStore(
+    combineReducers({heroes, filters}), 
+    compose(applyMiddleware(stringMiddleware), 
+        window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__())
+    
+    );
+
+export default store;
+
